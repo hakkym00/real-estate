87d385431ab63831665e1db6747afcd455e0124a
hakkym <akboiy50@gmail.com>
2022-10-09 12:19:29 +0100

    second commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -8,12 +8,6 @@ export const fetchApi = async (url) => {
       "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
-    // headers: {
-    //   "x-rapidapi-host": "bayut.p.rapidapi.com",
-    //   "x-rapidapi-key":
-    //     "1858132b7dmsh2dd9693f887b039p1d412fjsnac4c515efb4f" ||
-    //     process.env.NEXT_PUBLIC_RAPID_API_KEY,
-    // },
   });
 
   return data;
